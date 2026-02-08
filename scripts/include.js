c9ca2c8dea383bec0579ff919c7ae6bbf080a750
timmorgen <tim@censpace.com>
2026-02-07 19:35:19 -0600

    Update include.js

--- scripts/include.js
+++ scripts/include.js
@@ -1,7 +1,13 @@
-fetch('/includes/footer.html')
+fetch("/includes/header.html")
   .then(res => res.text())
   .then(html => {
-    document.getElementById('site-footer').innerHTML = html;
+    document.getElementById("site-header").innerHTML = html;
+  });
+
+fetch("/includes/footer.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("site-footer").innerHTML = html;
   });
 
 
